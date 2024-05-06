1b526243f503aa800ee2e9068c18fe19c068abd0
Markus Reinert <markus-reinert@web.de>
2024-05-06 17:22:27 +0200

    Add option to get volume and tracer bulk values at once
    Previously, it was necessary to calculate the bulk values for volume
    transport and tracer transport separately.  It was possible to provide
    the indices of the bulk values of the first calculation to the second,
    which reduced the number of computations.  However, with this method,
    the information got lost which bulk value corresponds to inflow and
    which to outflow.  This can be a problem when the tracer has both
    positive and negative values, like temperature in high latitudes.
    By computing both bulk values at once, even less computations are needed
    and it is clear which one corresponds to in- and which to outflow.
    Also, it is easier for the user, since only a single Dataset is returned
    instead of two separate Datasets with results.  This option is
    implemented with this commit.  As it is optional, it does not break any
    existing code.

--- pyTEF/calc.py
+++ pyTEF/calc.py
@@ -347,6 +347,7 @@ def sort_2dim(constructorTEF,
 # %% ../01_calc.ipynb 12
 def calc_bulk_values(coord,
                      Q,
+                     Qc=None,
                      Q_thresh=None,
                      index=None,
                      **kwargs):
@@ -362,6 +363,9 @@ def calc_bulk_values(coord,
 
         Qin_ar = np.zeros((Q.shape[0],10)) #10 is the dummy length
         Qout_ar = np.zeros((Q.shape[0],10))
+        if Qc is not None:
+            Qc_in_ar = np.zeros((Q.shape[0], 10))
+            Qc_out_ar = np.zeros((Q.shape[0], 10))
         divval_ar = np.zeros((Q.shape[0],11)) #if there are 10 transports there would be 11 dividing salinities
         indices = np.zeros((Q.shape[0],11))
 
@@ -383,14 +387,23 @@ def calc_bulk_values(coord,
                 #calculate transports etc.
             Q_in_m=[]
             Q_out_m=[]
+            if Qc is not None:
+                Qc_in = []
+                Qc_out = []
             index_del=[]
             i=0
             for i in range(len(ind)-1):
                 Q_i=-(Q[t,ind[i+1]]-Q[t,ind[i]])
+                if Qc is not None:
+                    Qc_i = -(Qc[t, ind[i+1]] - Qc[t, ind[i]])
                 if Q_i<0:
                     Q_out_m.append(Q_i)
+                    if Qc is not None:
+                        Qc_out.append(Qc_i)
                 elif Q_i > 0:
                     Q_in_m.append(Q_i)
+                    if Qc is not None:
+                        Qc_in.append(Qc_i)
                 else:
                     index_del.append(i)
                 i+=1
@@ -402,6 +415,11 @@ def calc_bulk_values(coord,
                 Qin_ar[t,i] = qq
             for i,qq in enumerate(Q_out_m):
                 Qout_ar[t,i] = qq
+            if Qc is not None:
+                for i,qq in enumerate(Qc_in):
+                    Qc_in_ar[t,i] = qq
+                for i,qq in enumerate(Qc_out):
+                    Qc_out_ar[t,i] = qq
             for i,ss in enumerate(div_val):
                 divval_ar[t,i] = ss
             for i,ss in enumerate(ind):
@@ -422,6 +440,9 @@ def calc_bulk_values(coord,
             "o": (["o"],np.arange(divval_ar.shape[1])),
         },
         )
+        if Qc is not None:
+            out["Qc_in"] = (["time", "m"], Qc_in_ar)
+            out["Qc_out"] = (["time", "n"], Qc_out_ar)
 
     else:
         #no time axis
@@ -443,14 +464,23 @@ def calc_bulk_values(coord,
             #calculate transports etc.
         Q_in_m=[]
         Q_out_m=[]
+        if Qc is not None:
+            Qc_in = []
+            Qc_out = []
         index_del=[]
         i=0
         for i in tqdm(range(len(ind)-1)):
             Q_i=-(Q[ind[i+1]]-Q[ind[i]])
+            if Qc is not None:
+                Qc_i = -(Qc[ind[i+1]] - Qc[ind[i]])
             if Q_i<0:
                 Q_out_m.append(Q_i)
+                if Qc is not None:
+                    Qc_out.append(Qc_i)
             elif Q_i > 0:
                 Q_in_m.append(Q_i)
+                if Qc is not None:
+                    Qc_in.append(Qc_i)
             else:
                 index_del.append(i)
             i+=1
@@ -470,6 +500,9 @@ def calc_bulk_values(coord,
             "o": (["o"],np.arange(len(div_val))),
         }
         )
+        if Qc is not None:
+            out["Qc_in"] = (["m"], Qc_in)
+            out["Qc_out"] = (["n"], Qc_out)
     return(out)
 
 # %% ../01_calc.ipynb 15
